22929ba982daebeed1417687f856ec4a2d6c09e7
ssnyder555 <ssnyder555@live.com>
2018-12-12 17:00:46 -0600

    create new park working

--- server.js
+++ server.js
@@ -1,25 +1,50 @@
-let express = require("express");
-let app     = express();
-let bodyParser = require("body-parser");
+let express = require("express"),
+ app        = express(),
+ bodyParser = require("body-parser"),
+ mongoose   = require("mongoose")
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs")
+mongoose.connect("mongodb://localhost/yelp_camp");
 
-let diskparks = [
-  {name: "Circle C Ranch", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvfrcX26C32NBg1MLYA7751pgnIThivKfSauaMyqJFZjBhIC69HA"},
-  {name: "Zilker Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj-pV2Kf7guw-FiJYxGocZbF8oJRo2NDO8_9_3lJjAvvqYp7GB"},
-  {name: "Mery More", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcQ6jo5oePP2YdCuJ_lU0BF4-T2o0nusAiI5EKNNyO4EGR8vol"},
-  {name: "Circle C Ranch", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvfrcX26C32NBg1MLYA7751pgnIThivKfSauaMyqJFZjBhIC69HA"},
-  {name: "Zilker Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj-pV2Kf7guw-FiJYxGocZbF8oJRo2NDO8_9_3lJjAvvqYp7GB"},
-  {name: "Mery More", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcQ6jo5oePP2YdCuJ_lU0BF4-T2o0nusAiI5EKNNyO4EGR8vol"}
- ]
+app.use(bodyParser.urlencoded({useNewUrlParser: true}));
+app.set("view engine", "ejs");
+
+// schema setup
+let diskparkSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+let Diskpark = mongoose.model("Diskpark", diskparkSchema);
+
+// Diskpark.create(
+//   {
+//     name: "Circle C Ranch",
+//     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvfrcX26C32NBg1MLYA7751pgnIThivKfSauaMyqJFZjBhIC69HA"
+//   },
+//   function(err, diskpark){
+//     if(err){
+//       console.log(err);
+//     } else {
+//       console.log("newly created park");
+//       console.log(diskpark);
+//     }
+//   }
+// )
+
 
 app.get("/", function(req, res){
   res.render("landing");
 })
 
 app.get("/diskparks", function(req, res){
-   res.render("diskparks", {diskparks: diskparks});
+   // res.render("diskparks", {diskparks: diskparks});
+   Diskpark.find({}, function(err, allDiskparks){
+     if(err){
+       console.log(err);
+     } else {
+       res.render("diskparks", {diskparks:allDiskparks})
+     }
+   })
 });
 
 // should show the form that will send the data to
@@ -32,11 +57,15 @@ app.post("/diskparks", function(req, res){
   let name  = req.body.name;
   let image = req.body.image;
   let newDiskpark = {name: name, image: image}
-  diskparks.push(newDiskpark);
-  // get daTA from form add to diskparks array
-  // redirect back to diskpark page
-  res.redirect("/diskparks");
-})
+  //create a new diskpark save to db
+  Diskpark.create(newDiskpark, function(err, newlyCreated){
+    if(err){
+      console.log(err);
+    } else {
+      res.redirect("/diskparks");
+    }
+  });
+});
 
 
 app.listen(3000, function(){
