3b0426a0a5c4b2e9ae359bf6a389cbec82366d9e
ssnyder555 <ssnyder555@live.com>
2018-12-17 10:50:26 -0600

    did some styling to show pages

--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
     {
         name: "Circle C",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4NarQ0A-ZIvpL2_Z0hAxBEURFYSpYCeEmmnmCHwa4ftvLQAWAsA",
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         name: "Merry More",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz7lYMLSjKlrBD4qN6PaOiEwYeZcTeQBg69SE5Ueg8hrUjsPL-QA",
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         name: "Organ Park",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTI26CuCNJdu6FLvPjoqZn35cz99xiG0LVKnKYBGrW3VOUeu6k",
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
 ]
 
