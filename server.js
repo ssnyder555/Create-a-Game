69c644d69f1c7b78003712fddc178ec9df5f5354
ssnyder555 <ssnyder555@live.com>
2018-12-17 15:53:31 -0600

    added login

--- server.js
+++ server.js
@@ -9,11 +9,23 @@ let express       = require("express"),
     User          = require("./models/user"),
     seedDB        = require("./seeds")
 
-mongoose.connect("mongodb://localhost/yelp_camp_v3");
+mongoose.connect("mongodb://localhost/yelp_camp_v3", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
+
+// Passport config:
+app.use(require("express-session")({
+  secret: "Frankie is so cute",
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -105,9 +117,42 @@ app.post("/campgrounds/:id/comments", function(req, res){
         });
        }
    });
-   //create new comment
-   //connect new comment to campground
-   //redirect campground show page
+});
+
+// Auth ROUTES
+// =================
+
+
+// show register form
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+// handle sign up logic
+app.post("/register", function(req, res){
+  const newUser = new User({username: req.body.username});
+  User.register( newUser, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.render("register")
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/campgrounds");
+    })
+  });
+});
+
+// show login form
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+// handling login logic
+
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/campgrounds",
+  falilureRedirect: "/login"
+}), function(req, res){
 });
 
 
